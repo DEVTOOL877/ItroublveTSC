193f4a427eb0d06728795c29e977b4c342c5ab43
HJX <73078933+DEVTOOL877@users.noreply.github.com>
2021-12-20 15:46:47 -0800

    Update Credits.Designer.cs

--- ItroublveTSC/Credits.Designer.cs
+++ ItroublveTSC/Credits.Designer.cs
@@ -116,7 +116,7 @@
             this.Advancedbtn.Name = "Advancedbtn";
             this.Advancedbtn.Size = new System.Drawing.Size(95, 101);
             this.Advancedbtn.TabIndex = 6344;
-            this.Advancedbtn.Text = "Advanced Options";
+            this.Advancedbtn.Text = "Advanced  options";
             this.Advancedbtn.UseVisualStyleBackColor = true;
             this.Advancedbtn.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -338,39 +338,6 @@
             this.CreditPnl.Size = new System.Drawing.Size(438, 454);
             this.CreditPnl.TabIndex = 6347;
             // 
-            // label11
-            // 
-            this.label11.AutoSize = true;
-            this.label11.Font = new System.Drawing.Font("Franklin Gothic Medium", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label11.ForeColor = System.Drawing.Color.White;
-            this.label11.Location = new System.Drawing.Point(8, 109);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(154, 17);
-            this.label11.TabIndex = 16;
-            this.label11.Text = "𝖉𝖊𝖚𝖘 | Developer + Friend";
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Font = new System.Drawing.Font("Franklin Gothic Medium", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.ForeColor = System.Drawing.Color.White;
-            this.label10.Location = new System.Drawing.Point(8, 245);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(241, 17);
-            this.label10.TabIndex = 15;
-            this.label10.Text = "Boosters / Beta Testers | Amazing People\r\n";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Franklin Gothic Medium", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(8, 58);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(208, 17);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "zFxbixn | ByteTools Open Source GUI";
-            // 
             // cred_ich
             // 
             this.cred_ich.AutoSize = true;
@@ -380,7 +347,7 @@
             this.cred_ich.Name = "cred_ich";
             this.cred_ich.Size = new System.Drawing.Size(288, 17);
             this.cred_ich.TabIndex = 1;
-            this.cred_ich.Text = "!㓐IchHackeNet㓐! | Creator + Developer + Friend";
+            this.cred_ich.Text = "000NQ#9660 | Creator + Developer + Open Source GUI ";
             // 
             // label9
             // 
@@ -391,74 +358,8 @@
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(181, 17);
             this.label9.TabIndex = 11;
-            this.label9.Text = "Dextority | Beta Tester + Friend";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Franklin Gothic Medium", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(8, 143);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(191, 17);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "RuinedUser | Developer + Friend";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Franklin Gothic Medium", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.ForeColor = System.Drawing.Color.White;
-            this.label8.Location = new System.Drawing.Point(8, 177);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(253, 17);
-            this.label8.TabIndex = 10;
-            this.label8.Text = "Blaststar | Developer + Beta Tester + Friend";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Franklin Gothic Medium", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(8, 160);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(166, 17);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "avoidfx | Developer + Friend";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Franklin Gothic Medium", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(8, 194);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(165, 17);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "Weeb | Beta Tester + Friend";
-            // 
-            // cred_kfcstaff
-            // 
-            this.cred_kfcstaff.AutoSize = true;
-            this.cred_kfcstaff.Font = new System.Drawing.Font("Franklin Gothic Medium", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cred_kfcstaff.ForeColor = System.Drawing.Color.White;
-            this.cred_kfcstaff.Location = new System.Drawing.Point(8, 92);
-            this.cred_kfcstaff.Name = "cred_kfcstaff";
-            this.cred_kfcstaff.Size = new System.Drawing.Size(254, 17);
-            this.cred_kfcstaff.TabIndex = 2;
-            this.cred_kfcstaff.Text = "!Hunolog | Beta Tester + Developer + Friend";
-            // 
-            // cntributor
-            // 
-            this.cntributor.AutoSize = true;
-            this.cntributor.Font = new System.Drawing.Font("Microsoft Tai Le", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cntributor.ForeColor = System.Drawing.Color.White;
-            this.cntributor.Location = new System.Drawing.Point(5, 12);
-            this.cntributor.Name = "cntributor";
-            this.cntributor.Size = new System.Drawing.Size(179, 34);
-            this.cntributor.TabIndex = 0;
-            this.cntributor.Text = "Contributors";
-            // 
+            this.label9.Text = "AhmedOsamaKing#5801 | Beta Tester + Friend";
+            //
             // Credits
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
